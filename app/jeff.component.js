7f22a862145fdd686122fa0a89c4423c8c021376
JeffreyMolina <jeffreymolina@gmail.com>
2016-06-29 10:34:33 -0700

    Added Wikipedia search

--- app/jeff.component.js
+++ app/jeff.component.js
@@ -1,4 +1,4 @@
-System.register(['@angular/core', './testItemService'], function(exports_1, context_1) {
+System.register(['@angular/core', '@angular/common', './testItemService', 'rxjs/add/operator/map', 'rxjs/add/operator/debounceTime', 'rxjs/add/operator/distinctUntilChanged', 'rxjs/add/operator/switchMap', './wikipediaService', '@angular/http'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,32 +10,52 @@ System.register(['@angular/core', './testItemService'], function(exports_1, cont
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, testItemService_1;
+    var core_1, common_1, testItemService_1, wikipediaService_1, http_1;
     var JeffComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             },
+            function (common_1_1) {
+                common_1 = common_1_1;
+            },
             function (testItemService_1_1) {
                 testItemService_1 = testItemService_1_1;
+            },
+            function (_1) {},
+            function (_2) {},
+            function (_3) {},
+            function (_4) {},
+            function (wikipediaService_1_1) {
+                wikipediaService_1 = wikipediaService_1_1;
+            },
+            function (http_1_1) {
+                http_1 = http_1_1;
             }],
         execute: function() {
             JeffComponent = (function () {
-                function JeffComponent(testItemService) {
+                function JeffComponent(testItemService, wikipediaService) {
                     this.testItemService = testItemService;
+                    this.wikipediaService = wikipediaService;
+                    this.term = new common_1.Control();
                     this.testItems = [];
                 }
                 JeffComponent.prototype.ngOnInit = function () {
+                    var _this = this;
                     this.testItems = this.testItemService.getTestItems();
+                    this.items = this.term.valueChanges
+                        .debounceTime(400)
+                        .distinctUntilChanged()
+                        .switchMap(function (term) { return _this.wikipediaService.search(term); });
                 };
                 JeffComponent = __decorate([
                     core_1.Component({
                         selector: 'jeff-component',
-                        template: "\n    <h1>This is the Awesome Jeff Component</h1>\n    <li *ngFor='let testItem of testItems'>\n        <div>{{testItem.testItemName}}</div>\n        <div>jeff was here</div>\n    </li>\n  ",
-                        providers: [testItemService_1.TestItemService]
+                        template: "\n    <h1>This is the Awesome Jeff Component</h1>\n    <li *ngFor='let testItem of testItems'>\n        <div>{{testItem.testItemName}}</div>\n        <div>jeff was here</div>\n    </li>\n    <div>\n        <h2>Wikipedia Search</h2>\n        <input type=\"text\" [ngFormControl]=\"term\" />\n        <ul>\n            <li *ngFor=\"let item of items | async\">{{item}}</li>\n        </ul>\n    </div>\n  ",
+                        providers: [wikipediaService_1.WikipediaService, http_1.JSONP_PROVIDERS]
                     }), 
-                    __metadata('design:paramtypes', [testItemService_1.TestItemService])
+                    __metadata('design:paramtypes', [testItemService_1.TestItemService, wikipediaService_1.WikipediaService])
                 ], JeffComponent);
                 return JeffComponent;
             }());
--- app/jeff.component.ts
+++ app/jeff.component.ts
@@ -1,17 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import {Control} from '@angular/common';
 import {TestItem} from './testItem';
 import {TestItemService} from './testItemService';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/switchMap';
+import {WikipediaService} from './wikipediaService';
+import {JSONP_PROVIDERS} from '@angular/http';
+
 
 @Component({
   selector: 'jeff-component',
-  templateUrl: './jeff.component.html'
+  templateUrl: './jeff.component.html',
+  providers: [WikipediaService, JSONP_PROVIDERS]
 })
 export class JeffComponent implements OnInit {
-    public constructor(private testItemService : TestItemService) {        
+
+    private items: Observable<Array<string>>;
+    private term = new Control();
+
+    public constructor(private testItemService: TestItemService, private wikipediaService: WikipediaService) {       
     }
 
     public testItems: TestItem[] = [];
     ngOnInit() {
         this.testItems = this.testItemService.getTestItems();
+        this.items = this.term.valueChanges
+            .debounceTime(400)
+            .distinctUntilChanged()
+            .switchMap(term => this.wikipediaService.search(term));
     }
 }
